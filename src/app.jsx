19a0fb0f22434340d02e9c1b4687dc548d08454d
steffakasid <steffen.rumpf@withouthat.org>
2020-10-20 12:20:44 +0200

    Add all images into one

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,56 @@
+(function main() {
+    const portrait = fourInOne(13, 19.5, 'four-in-one-portrait')
+    const landscape = fourInOne(19.5, 13, 'four-in-one-landscape')
+
+    const files = app.openDialog()
+
+    if (!files) {
+        alert('No files selected!')
+    } else {
+        const sortedFiles = sortFiles(files)
+
+        //portrait.processFiles(sortedFiles.portraitFiles)
+        landscape.processFiles(sortedFiles.landscapeFiles)
+    }
+})()
 
 function fourInOne(width, height, name) {
-  return {
-    width: width,
-    height: height,
-    miniWidth: width / 2 - 0.5,
-    miniheight: height / 2 - 0.5,
-    name: name
-  }
+    var miniWidth = width / 2 - 0.5
+    var miniHeight = height / 2 - 0.5
+    var base = app.documents.add(new UnitValue(width, "cm"), new UnitValue(height, "cm"), 300, name, NewDocumentMode.RGB, DocumentFill.TRANSPARENT)
+    return {
+        width: width,
+        height: height,
+        miniWidth: miniWidth,
+        miniHeight: miniHeight,
+        base: base,
+        name: name,
+        processFiles: function(docs) {
+            for (var i = 0; i < docs.length; i++) {
+                var doc = docs[i]
+
+                app.activeDocument = doc
+
+                doc.resizeImage(miniWidth, miniHeight, 300)
+
+                doc.activeLayer.copy()
+
+                app.activeDocument = base
+
+                base.paste()
+
+                //base.artLayers[0].translate(-10, -10)
+            }
+        }
+    }
 }
-
-
-
-const portrait = fourInOne(13, 19.5, 'four-in-one-portrait')
-const landscape = fourInOne(19.5, 13, 'four-in-one-landscape')
-
-const landscapeBaseDoc = app.documents.add(new UnitValue(landscape.height, "cm"), new UnitValue(landscape.width, "cm"), 300, landscape.name, NewDocumentMode.RGB, DocumentFill.TRANSPARENT)
-const portraitBaseDoc = app.documents.add(new UnitValue(portrait.height, "cm"), new UnitValue(portrait.height, "cm"), 300, portrait.name, NewDocumentMode.RGB, DocumentFill.TRANSPARENT)
 
 function sortFiles(files) {
     const portraitFiles = []
     const landscapeFiles = []
 
     for (var i = 0; i < files.length; i++) {
-        const file = files[i]
+        var file = open(files[i])
         if (file.height.value > file.width.value) {
             portraitFiles.push(file)
         } else {
@@ -35,21 +63,3 @@ function sortFiles(files) {
         landscapeFiles: landscapeFiles
     }
 }
-
-const files = app.openDialog()
-
-const fileOne = open(files[0])
-
-const sortedFiles = sortFiles(files)
-
-if (fileOne.height.value > fileOne.width.value) {
-    fileOne.resizeImage(6, 9, 300)
-} else {
-    fileOne.resizeImage(9, 6, 300)
-}
-
-fileOne.activeLayer.copy()
-
-app.activeDocument = portraitBase
-
-portraitBase.paste()
