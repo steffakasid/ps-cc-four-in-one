db6539900c2088066497ce83473a3ddd08925fe4
steffakasid <steffen.rumpf@withouthat.org>
2020-10-21 09:45:33 +0200

    Try saving images

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,12 @@
+var inputFolder
+var outputFolder
+
 (function main() {
     const portrait = fourInOne(13, 19.5, 'four-in-one-portrait')
     const landscape = fourInOne(19.5, 13, 'four-in-one-landscape')
 
+    inputFolder = Folder.selectDialog("Select a folder with images!")
+    outputFolder = Folder.selectDialog("Select a folder for the output files!")
     const files = app.openDialog()
 
     if (!files) {
@@ -9,7 +14,7 @@
     } else {
         const sortedFiles = sortFiles(files)
 
-        //portrait.processFiles(sortedFiles.portraitFiles)
+        portrait.processFiles(sortedFiles.portraitFiles)
         landscape.processFiles(sortedFiles.landscapeFiles)
     }
 })()
@@ -38,11 +43,34 @@ function fourInOne(width, height, name) {
                 app.activeDocument = base
 
                 base.paste()
+
+                doc.close(SaveOptions.DONOTSAVECHANGES)
+
+                if (i % 4 === 0 || i === docs.length - 1) {
+                    if (base.artLayers.length >= 1) {
+                        base.artLayers[0].translate(miniWidth * -1 / 2 - 0.5, miniHeight * -1 / 2 - 0.5)
+                    }
+                    if (base.artLayers.length >= 2) {
+                        base.artLayers[1].translate(miniWidth * -1 / 2 - 0.5, miniHeight / 2 + 0.5)
+                    }
+                    if (base.artLayers.length >= 3) {
+                        base.artLayers[2].translate(miniWidth / 2 + 0.5, miniHeight * -1 / 2 - 0.5)
+                    }
+                    if (base.artLayers.length >= 4) {
+                        base.artLayers[3].translate(miniWidth / 2 + 0.5, miniHeight / 2 + 0.5)
+                    }
+
+                    var jpegOptions = new JPEGSaveOptions()
+                    jpegOptions.quality = 10
+
+                    base.info.author = 'ps-cc-four-in-one-script'
+                    base.flatten()
+                    base.bitsPerChannel = BitsPerChannelType.EIGHT
+                    base.saveAs(new File(outputFolder + "/Output"+i+".jpg"), jpegOptions)
+                    base.close(SaveOptions.DONOTSAVECHANGES)
+                    base = app.documents.add(new UnitValue(width, "cm"), new UnitValue(height, "cm"), 300, name, NewDocumentMode.RGB, DocumentFill.TRANSPARENT)
+                }
             }
-            base.artLayers[0].translate(miniWidth * -1 / 2 - 0.5, miniHeight * -1 / 2 - 0.5)
-            base.artLayers[1].translate(miniWidth * -1 / 2 - 0.5, miniHeight / 2 + 0.5)
-            base.artLayers[2].translate(miniWidth / 2 + 0.5, miniHeight * -1 / 2 - 0.5)
-            base.artLayers[3].translate(miniWidth / 2 + 0.5, miniHeight / 2 + 0.5)
         }
     }
 }
